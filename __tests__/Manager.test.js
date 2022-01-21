aadc9c0138f7c65971fbfd91c7c1c508036c2798
reinholz36 <dreinholz28@gmail.com>
2022-01-21 17:45:50 -0500

    updated bootstrap styling

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,11 @@
 const Manager = require('../lib/Manager');
 
 test('test for Manager methods', () => {
-    const manager = new Manager('Dani', '01', '(999) 999-9999', 'dreinholz28@gmail.com')
+    const manager = new Manager('Dani', '01', 'dreinholz28@gmail.com', '(999) 999-9999')
 
     expect(manager.getName()).toBe('Dani');
     expect(manager.getRole()).toBe('Manager');
     expect(manager.getId()).toBe('01');
-    expect(manager.getPhone()).toBe('(999) 999-9999');
     expect(manager.getEmail()).toBe('dreinholz28@gmail.com');
+    expect(manager.getPhone()).toBe('(999) 999-9999');
 })
